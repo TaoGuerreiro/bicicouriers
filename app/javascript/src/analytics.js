7c0a4e5b5b63c69efde39aa8e9e92057fb87a064
Florent Guilbaud <florent.guilbaud@gmail.com>
2021-08-05 22:35:40 +0200

    adding gtag in application.js

---
+++ app/javascript/src/analytics.js
@@ -0,0 +1,15 @@
+
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+
+gtag('js', new Date());
+
+document.addEventListener("turbolinks:load", function(event) {
+  gtag('config', 'UA-126883810-1', {
+    page_location: event.data.url,
+    page_path: event.srcElement.location.pathname,
+    page_title: event.srcElement.title
+  });
+});
+
+export default gtag
