ceb29f877a65dfb08781d1b42d3d7b74fc7a07c9
Florent Guilbaud <florent.guilbaud@gmail.com>
2021-08-14 16:20:04 +0200

    adding slack message from contact

---
+++ app/jobs/send_message_to_slack_admin_job.rb
@@ -0,0 +1,13 @@
+class SendMessageToSlackAdminJob < ApplicationJob
+  queue_as :default
+
+  def perform(text)
+    return unless ENV['SLACK_COMMERCE_HOOK'].present?
+
+    HTTParty.post(
+      ENV['SLACK_COMMERCE_HOOK'],
+      body: text.to_json,
+      headers: { 'Content-Type' => 'application/json' }
+    )
+  end
+end
--- app/reflexes/contact_reflex.rb
+++ app/reflexes/contact_reflex.rb
@@ -6,6 +6,8 @@ class ContactReflex < ApplicationReflex
     @contact.city = City.first
     if @contact.save
       morph "#notifications", render(NotificationComponent.new(type: 'success', data: {timeout: 10, title: 'Message reçu !', body: "Nous avons pris note de votre message, s'il nous manque des détails, nous vous recontacterons.", countdown: true }))
+      morph "#contact_form", render(ContactComponent.new(contact: Contact.new))
+      SendMessageToSlackAdminJob.perform_now(slack_message(@contact))
     else
       morph "#contact_form", render(ContactComponent.new(contact: @contact))
       morph "#notifications", render(NotificationComponent.new(type: 'error', data: {timeout: 10, title: 'Oups !', body: "Il manque quelque chose...", countdown: true }))
@@ -14,6 +16,42 @@ class ContactReflex < ApplicationReflex
 
 
   private
+
+  def slack_message(contact)
+    {
+      "blocks": [
+        {
+          "type": "section",
+          "text": {
+            "type": "mrkdwn",
+            "text": "*Nouveau message de #{contact.name}*"
+          }
+        },
+        {
+          "type": "section",
+          "text": {
+            "type": "mrkdwn",
+            "text": "#{contact.message}"
+          }
+        },
+        {
+          "type": "section",
+          "text": {
+            "type": "mrkdwn",
+            "text": "#{contact.phone}"
+          }
+        },
+        {
+          "type": "section",
+          "text": {
+            "type": "mrkdwn",
+            "text": "#{contact.email}"
+          }
+        }
+      ]
+    }
+
+  end
 
   def contact_params
     params.require(:contact).permit(:name, :email, :phone, :message, :nickname)
