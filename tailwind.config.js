5be5f42cde79b1ca739d8e164c717048792e4d82
Florent Guilbaud <florent.guilbaud@gmail.com>
2021-08-18 16:35:17 +0200

    cards wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
     purge: {
       // Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
@@ -11,16 +13,35 @@ module.exports = {
     },
     darkMode: false, // or 'media' or 'class'
     theme: {
+      scale: {
+        '101': '1.01',
+        '102': '1.02',
+        '0': '0',
+        '25': '.25',
+        '50': '.5',
+        '75': '.75',
+        '90': '.9',
+        '95': '.95',
+        '100': '1',
+        '105': '1.05',
+        '110': '1.1',
+        '125': '1.25',
+        '150': '1.5',
+        '200': '2',
+      },
       minHeight: {
          '96': '24rem',
          '64': '16rem',
-       },
-       minWidth: {
+      },
+      minWidth: {
         '96': '24rem',
         '64': '16rem',
         '72': '18rem',
       },
       extend: {
+        animation: {
+          'spin-slow': 'spin 3s linear infinite',
+        },
         backgroundImage: theme => ({
           'bridge-image': "url('../../assets/images/story/story-image.png')",
           'bridge-large-image': "url('../../assets/images/velo-sur-pont-nantes.png')"
@@ -31,6 +52,7 @@ module.exports = {
           'pink-bici': '#FF016C',
           'pink-bici-dark': '#D8005A',
           'yellow-bici': '#FFCC00',
+          orange: colors.orange,
         },
       },
     },
